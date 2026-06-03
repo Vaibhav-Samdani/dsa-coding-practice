f37c80040c0afa48e2411960a4a4b72a30327f7a
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-06-03 11:51:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode mid = getMid(head);
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return mergeTwoList(left,right);
+    }
+
+    ListNode getMid(ListNode node){
+        if(node == null && node.next == null) return node;
+        ListNode slow = node;
+        ListNode fast = node;
+
+
+        while(fast.next != null && fast.next.next != null){ 
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode midPrev = slow;
+        ListNode mid = slow.next;
+
+        midPrev.next = null;
+
+        return mid;
+
+    }
+
+    ListNode mergeTwoList(ListNode left, ListNode right){
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while(left != null && right != null){
+            if(left.val <= right.val){
+                temp.next = left;
+                left = left.next;
+                temp = temp.next;
+            }else{
+                temp.next = right;
+                right = right.next;
+                temp = temp.next;
+            }
+        }
+
+        while(left != null){
+            temp.next = left;
+                left = left.next;
+                temp = temp.next;
+        }
+        while(right != null){
+            temp.next = right;
+                right = right.next;
+                temp = temp.next;
+        }
+
+        return dummy.next;
+    }
+}
