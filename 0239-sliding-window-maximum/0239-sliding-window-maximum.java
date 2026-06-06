da992adbcfd24bd34648d806159f0e30bb9c7126
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-06-07 04:46:54 +0530

    Time: 30 ms (71.48%), Space: 143.4 MB (87.61%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -3,22 +3,32 @@ class Solution {
         int n = nums.length;
 
         Deque<Integer> dq = new ArrayDeque<>();
+        // Deque is better option here as it is double-ended queue;
+        // Can be used as Stack;
+        // can also be implemented using LinkedList;
 
         int ans[] = new int[n-k+1];
+        // n-k+1 because k-1 values have no group
+
         int idx=0;
 
         for(int i = 0; i<n;i++){
+
             while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i] ){
+                // Remove last values on the basis of the new value if new value is greater than the prev at last end;
                 dq.pollLast();
             }
 
             while(!dq.isEmpty() && dq.peekFirst() <= i-k ){
+                // Remove first element if window exceed the value of k;
                 dq.pollFirst();
             }
             
+            // add element and back;
             dq.offerLast(i);
 
             if(i+1 >= k){
+                // PeekFirst contains maximum value's index;
                 ans[idx] = nums[dq.peekFirst()];
                 idx++;
             }
