4840b5b5f51acbada9315311826cb62753598c2e
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-06-10 09:53:20 +0530

    Time: 0 ms (100%), Space: 54.3 MB (8.67%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -5,14 +5,15 @@ class Solution {
             findJudge(2, new int[][]{{1,2}});
         }
     }
+
     public static int findJudge(int n, int[][] trust) {
 
         int[] in = new int[n + 1];
         int[] out = new int[n + 1];
 
-        for (int i = 0; i < trust.length; i++) {
-            out[trust[i][0]]++;
-            in[trust[i][1]]++;
+        for (int[] arr : trust) {
+            out[arr[0]]++;
+            in[arr[1]]++;
         }
 
         int jud = -1;
