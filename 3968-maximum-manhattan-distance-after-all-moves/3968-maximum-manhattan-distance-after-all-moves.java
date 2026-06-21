b6d500139770de2da167bc49d3de5d793158b7f9
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-06-21 23:25:30 +0530

    Time: 5 ms (100%), Space: 48 MB (44.46%) - LeetHub

--- 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
@@ -1,18 +1,10 @@
 class Solution {
     public int maxDistance(String moves) {
-        int x = 0;
-        int y = 0;
-        int b = 0;
+        int counts[] = new int[128];
 
-        for(int i = 0; i<moves.length();i++){
-            char ch = moves.charAt(i);
-            if(ch == 'U') x++;
-            else if(ch == 'D') x--;
-            else if(ch == 'L') y++;
-            else if(ch == 'R') y--;
-            else b++;
+        for(int i=0;i<moves.length();i++){
+            counts[moves.charAt(i)]++;
         }
-
-        return Math.abs(x) + Math.abs(y) + b;
+        return Math.abs(counts['U'] - counts['D'])+Math.abs(counts['L']-counts['R'])+ counts['_'];
     }
 }
