98add0b37714601e06201c7ad4263ab97c9c7bee
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-03 00:04:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,51 +1,52 @@
 class Solution {
-    boolean cycle = false;
-    public boolean isCycle(int V, int[][] edges) {
-        
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        
-        boolean[] vis = new boolean[V];
-        
-        for(int i = 0 ; i < V; i++){
-            adj.add(new ArrayList<>());
-        }
-        
-        for(int i = 0; i < edges.length; i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-        
-        for(int i =0; i<V;i++){
-            if(!vis[i]){
-                
-        helper(adj,-1,i,vis);
+	public boolean isCycle(int V, int[][] edges) {
+		
+		ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+		
+		boolean[] vis = new boolean[V];
+		
+		for (int i = 0; i<V; i++) {
+			adj.add(new ArrayList<>());
+		}
+		
+		for (int i = 0; i < edges.length; i++) {
+			int u = edges[i][0];
+			int v = edges[i][1];
+			
+			adj.get(u).add(v);
+			adj.get(v).add(u);
+		}
+		
+		for (int i = 0; i < V; i++) {
+			if (!vis[i]) {
+				if (helper(vis, adj, -1, i)) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+		
+	}
+	
+	boolean helper(boolean[] vis, ArrayList<ArrayList<Integer>> adj, int parent, int curr) {
+
+    vis[curr] = true;
+
+    for (int i = 0; i < adj.get(curr).size(); i++) {
+
+        int val = adj.get(curr).get(i);
+
+        if (!vis[val]) {
+            if (helper(vis, adj, curr, val)) {
+                return true;
             }
         }
-        
-        
-        return cycle;
-        
-        
-    }
-    
-    void helper(ArrayList<ArrayList<Integer>> adj, int parent, int curr, boolean[]  vis){
-        vis[curr] = true;
-        
-        
-        for(int i = 0 ; i < adj.get(curr).size();i++){
-            int node = adj.get(curr).get(i);
-            
-            if(!vis[node]){
-                helper(adj,curr,node,vis);
-            }else if(node != parent){
-                cycle = true;
-                
-            }
+        else if (val != parent) {
+            return true;
         }
-        
-        
     }
+
+    return false;
+}
 }
