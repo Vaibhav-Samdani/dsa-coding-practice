1b3891aef888e852f363016b10c1c1c2d06ba40a
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-04 01:01:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,48 +1,51 @@
 class Solution {
-    public boolean isCyclic(int V, int[][] edges) {
-        int[] indeg = new int[V];
-        
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        
-        boolean[] vis = new boolean[V];
-        
-        for(int i = 0 ; i < V; i++){
-            adj.add(new ArrayList<>());
-        }
-        
-        for(int i = 0; i < edges.length; i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            
-            adj.get(u).add(v);
-            
-            indeg[v]++;
-        }
-        
-        Queue<Integer> q = new LinkedList<>();
-        
-        int cnt = 0;
-        
-        for(int i = 0; i < V; i++){
-            if(indeg[i] == 0){
-                q.offer(i);
-            }
-        }
-        
-        
-        while(!q.isEmpty()){
-            int u = q.poll();
-            
-            cnt++;
-            for(int i : adj.get(u)){
-                indeg[i]--;
-                if(indeg[i] == 0){
-                    q.offer(i);
-                }
-            }
-        }
-        
-        return !(cnt == V);
-        
-    }
+	public boolean isCyclic(int V, int[][] edges) {
+		
+		ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+		
+		boolean[] vis = new boolean[V];
+		boolean[] isRec = new boolean[V];
+		
+		for (int i = 0; i < V; i++) {
+			adj.add(new ArrayList<>());
+		}
+		
+		for (int i = 0; i < edges.length; i++) {
+			
+			int u = edges[i][0];
+			int v = edges[i][1];
+			
+			adj.get(u).add(v);
+		}
+		
+		for (int i = 0; i<adj.size(); i++) {
+			
+			if (!vis[i] && helper(adj, vis, isRec, i))
+				return true;
+			
+		}
+		
+		return false;
+	}
+	
+	boolean helper(ArrayList<ArrayList<Integer>> adj, boolean vis[], boolean isRec[], int curr) {
+		vis[curr] = true;
+		isRec[curr] = true;
+		
+		for (int i = 0; i < adj.get(curr).size(); i++) {
+			int val = adj.get(curr).get(i);
+			if (isRec[val]) {
+				return true;
+			}
+			if (!vis[val]) {
+				if (helper(adj, vis, isRec, val))
+					return true;
+				
+			}
+		}
+		
+		isRec[curr] = false;
+		
+		return false;
+	}
 }
