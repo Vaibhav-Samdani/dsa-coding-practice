d66a546a4b398b287fd23099c88904ea00739f30
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-17 00:54:20 +0530

    Time: 19 ms (91.44%), Space: 54.1 MB (20.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,24 +2,20 @@ class Solution {
     public int longestCommonSubsequence(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        int[][] dp = new int[n][m];
-        for(int i = 0; i<n;i++){
-            Arrays.fill(dp[i],-1);
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if(s1.charAt(i) == s2.charAt(j)){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }else{
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
         }
-        return helper(dp,s1,s2,0,0);
+
+        return dp[0][0];
     }
 
-    int helper(int[][] dp, String s1, String s2, int i, int j){
-        if(i == s1.length() || j == s2.length()) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s1.charAt(i) == s2.charAt(j)) return dp[i][j] = 1 + helper(dp,s1,s2,i+1,j+1);
-        else{
-            int skip1 = helper(dp,s1,s2,i+1,j);
-            int skip2 = helper(dp, s1,s2,i,j+1);
-
-            return dp[i][j] = Math.max(skip1,skip2);
-        }
-    }
+   
 }
