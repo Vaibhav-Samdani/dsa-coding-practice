a0cb6aab7fd7583f6cb0eb4353c5976e6b3dc785
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-16 22:36:42 +0530

    Time: 1 ms (60.83%), Space: 47 MB (77.84%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,50 +1,40 @@
 class Solution {
-
     public boolean isBipartite(int[][] graph) {
-
-        // 0  -> Red;
-        // 1  -> Green;
-        // -1 -> Uncolored;        
         int[] colors = new int[graph.length];
-
-        Arrays.fill(colors, -1);
-
-        for(int i =0; i<graph.length;i++){
+        Arrays.fill(colors,-1);
+        for(int i = 0 ; i<graph.length;i++){
             if(colors[i] == -1){
-                if(!bfs(graph,colors,i)){
+                if(!helper(graph,colors,i)){
                     return false;
                 }
             }
         }
         return true;
-
     }
 
-    boolean bfs(int[][] graph, int[] colors, int curr){
-        Queue<Integer> q = new ArrayDeque<>();
+    boolean helper(int[][] graph, int[] colors, int curr){
+        Queue<Integer> q = new LinkedList<>();
 
         q.offer(curr);
+
         colors[curr] = 0;
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int val = q.poll();
 
-            for (int neighbor : graph[val]) {
+            for(int neigh : graph[val]){
 
-                if (colors[neighbor] != -1) {
-                    if (colors[neighbor] == colors[val])
+                if(colors[neigh] != -1){
+                    if(colors[neigh] == colors[val]){
                         return false;
-                }
-
-                else if (colors[neighbor] == -1) {
-                    colors[neighbor] = 1 - colors[val];
-                    q.offer(neighbor);
+                    }
+                }else{
+                    colors[neigh] = 1 - colors[val];
+                    q.offer(neigh);
                 }
             }
         }
 
         return true;
-
     }
-
 }
