98f7a6673c06f8327b693de7ca6a33c67407397c
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-18 20:45:03 +0530

    Time: 1820 ms (5.1%), Space: 51.5 MB (56.92%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,42 +1,39 @@
 class Solution {
-    int[][] dp; 
+    boolean[][] dp;
+
     public boolean canCross(int[] stones) {
         int n = stones.length;
 
-        if (n > 1 && stones[1] != 1) return false;
-        dp = new int[stones.length+1][stones.length];
-        
-        for(int i = 0; i<stones.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
-
-        return solve(stones, 1, 1);
-    }
-
-    boolean solve(int[] stones, int k, int i) {
-        if (i >= stones.length) return false;
-        if (i == stones.length - 1) return true;
-        if(dp[k][i] != -1) return dp[k][i] == 1;
-
-        if (k <= 0 ) {
+        if (n > 1 && stones[1] != 1)
             return false;
+        dp = new boolean[n][n + 1];
+
+        for (int i = 0; i < n + 2; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i == stones.length - 1) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+    
+
+        for (int i = n-2; i >= 1; i--) {
+            for (int j = 1; j < n; j++) {
+                
+                int currPos = stones[i];
+                int j1 = Arrays.binarySearch(stones, currPos + j - 1);
+                int j2 = Arrays.binarySearch(stones, currPos + j);
+                int j3 = Arrays.binarySearch(stones, currPos + j + 1);
+
+                boolean t1 = j1 >= 0 && j != 1 ? dp[j1][j-1] : false;
+                boolean t2 = j2 >= 0 ? dp[j2][j] : false;
+                boolean t3 = j3 >= 0 ? dp[j3][j+1] : false;
+
+                dp[i][j] = t1 || t2 || t3;
+
+            }
         }
 
-        int currPos = stones[i];
-        int j1 = Arrays.binarySearch(stones, currPos + k - 1);
-        int j2 = Arrays.binarySearch(stones, currPos + k);
-        int j3 = Arrays.binarySearch(stones, currPos + k + 1);
-
-        boolean t1 = j1 >= 0 && k != 1 ? solve(stones, k - 1, j1) : false;
-        boolean t2 = j2 >= 0 ? solve(stones, k, j2) : false;
-        boolean t3 = j3 >= 0 ? solve(stones, k + 1, j3) : false;
-
-        if(t1 || t2 || t3){
-            dp[k][i] = 1;
-        }else{
-            dp[k][i] = 0;
-        }
-
-        return t1 || t2 || t3;
+        return dp[1][1];
     }
 }
