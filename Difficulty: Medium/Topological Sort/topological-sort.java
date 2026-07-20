31595cac9530c27c50c35b87aab84ce0133ac3a1
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-20 23:36:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,53 +1,43 @@
 class Solution {
-	public ArrayList<Integer> topoSort(int V, int[][] edges) {
-		// code here
-		
-		ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-		
-		boolean[] vis = new boolean[V];
-		
-		for (int i = 0; i<V; i++) {
-			adj.add(new ArrayList<>());
-		}
-		
-		for (int i = 0; i<edges.length; i++) {
-			int u = edges[i][0];
-			int v = edges[i][1];
-			
-			adj.get(u).add(v);
-		}
-		
-		Stack<Integer> st = new Stack<>();
-		
-		for (int i = 0; i<adj.size(); i++) {
-			if (!vis[i]) {
-				dfs(vis, adj, i,st);
-				// st.push(i);
-			}
-			
-		}
-		
-		ArrayList<Integer> ans = new ArrayList<>();
-		
-		while (!st.isEmpty()) {
-			ans.add(st.pop());
-		}
-		
-		return ans;
-		
-	}
-	
-	void dfs(boolean[] vis, ArrayList<ArrayList<Integer>> adj, int curr,Stack<Integer> st) {
-		vis[curr] = true;
-		
-		for (int i = 0; i<adj.get(curr).size(); i++) {
-			int val = adj.get(curr).get(i);
-			if (!vis[val]) {
-				dfs(vis, adj, val,st);
-			}
-		}
-		
-		st.push(curr);
-	}
-	
+    public ArrayList<Integer> topoSort(int V, int[][] edges) {
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        for(int i = 0 ; i < V; i++){
+            adj.add(new ArrayList<Integer>());
+        }
+        
+        int[] indeg = new int[V];
+        
+        for(int i = 0; i < edges.length;i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adj.get(u).add(v);
+            indeg[v]++;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        
+        for(int i = 0; i<V;i++){
+            if(indeg[i]==0) q.offer(i);
+        }
+        
+        
+        while(!q.isEmpty()){
+            int temp = q.poll();
+            
+            ans.add(temp);
+            
+            for(int neigh : adj.get(temp)){
+                indeg[neigh]--;
+                if(indeg[neigh] == 0){
+                    q.offer(neigh);
+                }
+            }
+        }
+        return ans;
+        
+    }
 }
