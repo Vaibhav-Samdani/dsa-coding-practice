9502fc7e0f918e1073623821888fe8a3531b598f
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-23 04:34:17 +0530

    Time: 1 ms (99.94%), Space: 46.2 MB (84.5%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,14 +1,23 @@
 class Solution {
-
     public boolean isValidSudoku(char[][] board) {
-        Set<String> st = new HashSet<>();
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
+
                 if (board[row][col] == '.') continue;
-                String rowKey = board[row][col] + "R" + row;
-                String colKey = board[row][col] + "C" + col;
-                String boxKey = board[row][col] + "B" + (row / 3) + (col / 3);
-                if (!st.add(rowKey) || !st.add(colKey) || !st.add(boxKey)) return false;
+
+                int num = board[row][col] - '1';
+                int box = (row / 3) * 3 + (col / 3);
+
+                if (rows[row][num] || cols[col][num] || boxes[box][num])
+                    return false;
+
+                rows[row][num] = true;
+                cols[col][num] = true;
+                boxes[box][num] = true;
             }
         }
 
