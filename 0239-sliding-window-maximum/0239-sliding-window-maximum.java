7f0f3918b37a1281e5317c45198ea3000e4a7d1d
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-28 02:52:48 +0530

    Time: 29 ms (85.49%), Space: 147.6 MB (48.23%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,49 +1,29 @@
 class Solution {
-    static{
-        int [] ans = new int[]{5,6,7};
 
-        for(int i =0; i< 301;i++){
-            maxSlidingWindow(ans,2);
-        }
-       
-    }
-    public static int[] maxSlidingWindow(int[] nums, int k) {
+    public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
 
+        int[] ans = new int[n - k + 1];
+        int p = 0;
+
         Deque<Integer> dq = new ArrayDeque<>();
-        // Deque is better option here as it is double-ended queue;
-        // Can be used as Stack;
-        // can also be implemented using LinkedList;
 
-        int ans[] = new int[n-k+1];
-        // n-k+1 because k-1 values have no group
+        for (int i = 0; i < n; i++) {
+            while (!dq.isEmpty() && dq.peek() <= i - k) {
+                dq.poll();
+            }
 
-        int idx=0;
-
-        for(int i = 0; i<n;i++){
-
-            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i] ){
-                // Remove last values on the basis of the new value if new value is greater than the prev at last end;
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
                 dq.pollLast();
             }
 
-            while(!dq.isEmpty() && dq.peekFirst() <= i-k ){
-                // Remove first element if window exceed the value of k;
-                dq.pollFirst();
-            }
-            
-            // add element and back;
-            dq.offerLast(i);
+            dq.offer(i);
 
-            if(i+1 >= k){
-                // PeekFirst contains maximum value's index;
-                ans[idx] = nums[dq.peekFirst()];
-                idx++;
+            if (i >= k - 1) {
+                ans[p++] = nums[dq.peek()];
             }
-
         }
 
         return ans;
-
     }
 }
