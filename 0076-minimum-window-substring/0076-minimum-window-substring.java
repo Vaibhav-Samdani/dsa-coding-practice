9e6caa2825c3d8745901823c077b09d6a01061b9
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-28 01:25:03 +0530

    Time: 16 ms (56.63%), Space: 47.2 MB (47.23%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,50 +1,47 @@
 class Solution {
+
     public String minWindow(String s, String t) {
-        
-        if(t.length() > s.length()){
-            return "";
-        }
-        
-        int[] mp = new int[256];
+        if (t.length() > s.length()) return "";
 
+        HashMap<Character, Integer> mp = new HashMap<>();
 
-
-        for (char ch : t.toCharArray()) {
-            mp[ch]++;
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
         }
 
         int i = 0;
-        int count = 0, minI = 0, minJ = Integer.MAX_VALUE;
+        int minI = 0, minJ = Integer.MAX_VALUE;
+        int count = 0;
 
         for (int j = 0; j < s.length(); j++) {
             char ch = s.charAt(j);
-
-            mp[ch]--;
-            if(mp[ch] >= 0 ){
-                count++;
+            if(mp.containsKey(ch)){
+                mp.put(ch,mp.get(ch)-1);
+                if(mp.get(ch) >= 0){
+                    count++;
+                }
             }
 
             while(count == t.length()){
-                char ch2 = s.charAt(i);
-                
-                if((minJ - minI) >= (j-i) ){
-                    minJ = j;
+                if((minJ - minI) > (j-i)){
                     minI = i;
+                    minJ = j;
                 }
-                
-                mp[ch2]++;
-                if(mp[ch2] > 0){
-                    count--;
+
+                if(mp.containsKey(s.charAt(i))){
+                    mp.put(s.charAt(i),mp.get(s.charAt(i))+1);
+                    if(mp.get(s.charAt(i)) > 0){
+                        count--;
+                    }
                 }
 
                 i++;
-                
             }
         }
 
         if(minJ == Integer.MAX_VALUE) return "";
 
-        return s.substring(minI, minJ+1);
-
+        return s.substring(minI,minJ+1);
     }
 }
