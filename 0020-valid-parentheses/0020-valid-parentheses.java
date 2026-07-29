a8881b2e7fc793e231f735d58ac1dc64fda20298
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-07-30 02:44:26 +0530

    Time: 3 ms (86.12%), Space: 43.4 MB (25.94%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) return false;
+
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '('){
+                st.push(')');
+            }else if(ch == '{'){
+                st.push('}');
+            }else if(ch == '['){
+                st.push(']');
+            }else{
+                if(!st.isEmpty() && st.peek() == ch){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
