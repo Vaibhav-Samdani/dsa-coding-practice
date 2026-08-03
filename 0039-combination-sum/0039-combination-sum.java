a6f53eb01fda94668747e44aa40289c2cff69b4c
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-04 04:30:25 +0530

    Time: 4 ms (13.05%), Space: 45.5 MB (83.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,26 @@
 class Solution {
     List<List<Integer>> ans;
+
     public List<List<Integer>> combinationSum(int[] nums, int target) {
         ans = new ArrayList<>();
-        // Arrays.sort(nums);
-        helper(nums,target,0,new ArrayList<>());
+        Arrays.sort(nums);
+        helper(nums, target, 0, new ArrayList<>());
         return ans;
     }
 
-    void helper(int[] nums, int target, int i, List<Integer> curr){
-        if(target == 0){
+    void helper(int[] nums, int target, int i, List<Integer> curr) {
+        if (target == 0) {
             ans.add(new ArrayList<>(curr));
             return;
         }
-        if(target < 0){
-            return;
-        }
 
-        for(int j = i; j < nums.length;j++){
+        for (int j = i; j < nums.length; j++) {
+            if (target < 0) {
+                return;
+            }
             curr.add(nums[j]);
-            helper(nums,target-nums[j],j,curr);
-            curr.remove(curr.size()-1);
+            helper(nums, target - nums[j], j, curr);
+            curr.remove(curr.size() - 1);
         }
-
     }
 }
