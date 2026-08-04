9d571ec8590be574232a3556d89a2ccea38261cb
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-04 22:51:34 +0530

    Time: 3 ms (33.94%), Space: 46 MB (18.65%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -16,7 +16,7 @@ class Solution {
 
 
         for(int i = start; i<nums.length;i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+            if(i > start && nums[i] == nums[i-1]) continue;
             if(nums[i] > target) continue;
 
             curr.add(nums[i]);
