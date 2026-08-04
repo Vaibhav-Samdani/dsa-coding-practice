c39e4fa9fb69fd8ac923f226b2d98923ba1b597f
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-05 01:05:53 +0530

    Time: 2 ms (99.72%), Space: 44.9 MB (89.78%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -9,18 +9,16 @@ class Solution {
     }
 
     void solve(int[] nums, int start, List<Integer> curr) {
-        if (start == nums.length) {
+        // if (start == nums.length) {
             ans.add(new ArrayList<>(curr));
-            return;
+            // return;
+        // }
+
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            curr.add(nums[i]);
+            solve(nums, i + 1, curr);
+            curr.remove(curr.size() - 1);
         }
-
-        curr.add(nums[start]);
-        solve(nums, start + 1, curr);
-        curr.remove(curr.size() - 1);
-
-        int j = start + 1;
-        while (j < nums.length && nums[j] == nums[start]) j++;
-
-        solve(nums, j, curr);
     }
 }
