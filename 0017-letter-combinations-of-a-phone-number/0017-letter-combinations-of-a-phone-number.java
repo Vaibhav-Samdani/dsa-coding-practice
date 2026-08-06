47de6e12e239fe2010b4a4e4ad69f5132a66b679
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-07 04:49:50 +0530

    Time: 0 ms (100%), Space: 43.7 MB (37.78%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -4,21 +4,21 @@ class Solution {
     HashMap<Character, char[]> mp;
 
     public List<String> letterCombinations(String digits) {
-
         ans = new ArrayList<>();
+
+        if (digits.isEmpty()) return ans;
         mp = new HashMap<>();
 
-        mp.put('2',new char[]{'a','b','c'});
-        mp.put('3',new char[]{'d','e','f'});
-        mp.put('4',new char[]{'g','h','i'});
-        mp.put('5',new char[]{'j','k','l'});
-        mp.put('6',new char[]{'m','n','o'});
-        mp.put('7',new char[]{'p','q','r','s'});
-        mp.put('8',new char[]{'t','u','v'});
-        mp.put('9',new char[]{'w','x','y','z'});
-        
+        mp.put('2', new char[] { 'a', 'b', 'c' });
+        mp.put('3', new char[] { 'd', 'e', 'f' });
+        mp.put('4', new char[] { 'g', 'h', 'i' });
+        mp.put('5', new char[] { 'j', 'k', 'l' });
+        mp.put('6', new char[] { 'm', 'n', 'o' });
+        mp.put('7', new char[] { 'p', 'q', 'r', 's' });
+        mp.put('8', new char[] { 't', 'u', 'v' });
+        mp.put('9', new char[] { 'w', 'x', 'y', 'z' });
 
-        solve(digits,new StringBuilder(),0);
+        solve(digits, new StringBuilder(), 0);
 
         return ans;
     }
