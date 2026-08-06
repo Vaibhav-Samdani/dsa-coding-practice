bc05b23fd251a13fbd7dded4a9b13327758b1738
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-07 00:36:49 +0530

    Time: 0 ms (100%), Space: 44.5 MB (65.6%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,33 +1,29 @@
 class Solution {
-
     List<String> ans;
-
     public List<String> generateParenthesis(int n) {
         ans = new ArrayList<>();
-
-        solve(n,0,0, new StringBuilder());
-
+        solve(n,0,0,new StringBuilder());
         return ans;
     }
 
-    void solve(int n, int open, int close, StringBuilder curr) {
-        if (close == n && open == n) {
-            ans.add(curr.toString());
-            return;
+    void solve(int n , int open, int close, StringBuilder st){
+        if(n*2 == st.length()){
+            ans.add(st.toString());
         }
 
-        if(open < n){
-            curr.append('(');
-            solve(n,open+1,close,curr);
-            curr.deleteCharAt(curr.length()-1);
+        if(open < n) {
+            st.append('(');
+            solve(n,open+1,close,st);
+            st.deleteCharAt(st.length()-1);
         }
 
-        if(close < open){
-            curr.append(')');
-            solve(n,open,close+1,curr);
-            curr.deleteCharAt(curr.length()-1);
+        if(open > close) {
+            st.append(')');
+            solve(n,open,close+1,st);
+            st.deleteCharAt(st.length()-1);
         }
+
         
-        
+
     }
 }
