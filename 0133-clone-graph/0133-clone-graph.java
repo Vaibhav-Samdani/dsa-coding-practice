4b1862c17f73cc2053bf801b6486369dd7bce501
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-08 01:34:02 +0530

    Time: 29 ms (7.6%), Space: 44.5 MB (22.47%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -20,34 +20,34 @@ class Node {
 
 class Solution {
     HashMap<Node, Node> mp;
+
     public Node cloneGraph(Node node) {
         mp = new HashMap<>();
 
         Node temp = dfs(node);
 
         return temp;
-
     }
 
     Node dfs(Node node) {
-        if (node == null)
-            return node;
+        if (node == null) return node;
+
+        if (mp.containsKey(node)) return mp.get(node);
 
         Node temp = new Node(node.val);
-        mp.put(node,temp);
+        mp.put(node, temp);
 
         List<Node> neighbors = node.neighbors;
 
         for (int i = 0; i < neighbors.size(); i++) {
-            if(!mp.containsKey(neighbors.get(i))){
+            if (!mp.containsKey(neighbors.get(i))) {
                 dfs(neighbors.get(i));
             }
         }
 
         List<Node> newNeigh = new ArrayList<>();
 
-
-        for(int i = 0; i<neighbors.size();i++){
+        for (int i = 0; i < neighbors.size(); i++) {
             newNeigh.add(mp.get(neighbors.get(i)));
         }
 
