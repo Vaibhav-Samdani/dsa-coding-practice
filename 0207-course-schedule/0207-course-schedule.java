60c0714825d90420d915cef8fda0e2e4cee57c96
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-10 04:47:12 +0530

    Time: 6 ms (71.92%), Space: 47.2 MB (49.44%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,53 +1,47 @@
 class Solution {
-    public boolean canFinish(int V, int[][] edges) {
+    public boolean canFinish(int N, int[][] edges) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-		
-		boolean[] vis = new boolean[V];
-		
-		int[] indeg = new int[V];
-		
-		for (int i = 0; i<V; i++) {
-			adj.add(new ArrayList<>());
-		}
-		
-		for (int i = 0; i<edges.length; i++) {
-			int u = edges[i][0];
-			int v = edges[i][1];
-			
-			adj.get(v).add(u);
-		}
-		
-		ArrayList<Integer> ans = new ArrayList<>();
-		Queue<Integer> q = new LinkedList<>();
-		
-		for (int i = 0; i<adj.size(); i++) {
-			for (int val : adj.get(i)) {
-				indeg[val]++;
-			}
-		}
-		
-		for (int i = 0; i<V; i++) {
+
+        for (int i = 0; i < N; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        int[] indeg = new int[N];
+
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            indeg[u]++;
+
+            adj.get(v).add(u);
+        }
+
+        int count = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i<N; i++) {
 			if (indeg[i] == 0) {
 				q.offer(i);
 			}
 		}
-		
-		while (!q.isEmpty()) {
-			int u = q.poll();
-			
-			ans.add(u);
-			
-			for (int v: adj.get(u)) {
-				indeg[v]--;
-				
-				if (indeg[v] == 0) {
-					q.offer(v);
-				}
-			}
-		}
 
-        return ans.size() == V;
-		
-		// return true;
+
+        while (!q.isEmpty()) {
+            int val = q.poll();
+
+            count++;
+
+            for (int neigh : adj.get(val)) {
+                indeg[neigh]--;
+                if (indeg[neigh] == 0) {
+                    q.offer(neigh);
+                }
+            }
+        }
+
+        return count == N;
     }
+
 }
