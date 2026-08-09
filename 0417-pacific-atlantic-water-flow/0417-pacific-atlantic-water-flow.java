da73eabd450720ff6d0686b8b4ca78c266d20ab9
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-10 03:23:13 +0530

    Time: 13 ms (16.55%), Space: 47.8 MB (12.31%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,138 +1,49 @@
-class Solution {
+public class Solution {
+    private int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int ROWS = heights.length, COLS = heights[0].length;
+        boolean[][] pac = new boolean[ROWS][COLS];
+        boolean[][] atl = new boolean[ROWS][COLS];
 
-    public List<List<Integer>> pacificAtlantic(int[][] nums) {
+        Queue<int[]> pacQueue = new LinkedList<>();
+        Queue<int[]> atlQueue = new LinkedList<>();
 
-        int m = nums.length;
-        int n = nums[0].length;
+        for (int c = 0; c < COLS; c++) {
+            pacQueue.add(new int[]{0, c});
+            atlQueue.add(new int[]{ROWS - 1, c});
+        }
+        for (int r = 0; r < ROWS; r++) {
+            pacQueue.add(new int[]{r, 0});
+            atlQueue.add(new int[]{r, COLS - 1});
+        }
 
-        List<List<Integer>> ans = new ArrayList<>();
+        bfs(pacQueue, pac, heights);
+        bfs(atlQueue, atl, heights);
 
-        // 0 = unknown
-        // 1 = currently visiting
-        // 2 = can reach ocean
-        // 3 = cannot reach ocean
-        int[][] pState = new int[m][n];
-        int[][] aState = new int[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (dfsP(nums, i, j, pState) &&
-                    dfsA(nums, i, j, aState)) {
-
-                    ans.add(List.of(i, j));
+        List<List<Integer>> res = new ArrayList<>();
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (pac[r][c] && atl[r][c]) {
+                    res.add(Arrays.asList(r, c));
                 }
             }
         }
-
-        return ans;
+        return res;
     }
 
-    boolean dfsP(int[][] nums, int i, int j, int[][] state) {
-
-        // Reached Pacific
-        if (i < 0 || j < 0) {
-            return true;
-        }
-
-        // Already computed
-        if (state[i][j] == 2) {
-            return true;
-        }
-
-        if (state[i][j] == 3) {
-            return false;
-        }
-
-        // Cycle
-        if (state[i][j] == 1) {
-            return false;
-        }
-
-        state[i][j] = 1;
-
-        int[][] dirs = {
-            {-1, 0},
-            {1, 0},
-            {0, -1},
-            {0, 1}
-        };
-
-        for (int[] d : dirs) {
-
-            int ni = i + d[0];
-            int nj = j + d[1];
-
-            if (ni >= 0 && ni < nums.length &&
-                nj >= 0 && nj < nums[0].length &&
-                nums[ni][nj] <= nums[i][j]) {
-
-                if (dfsP(nums, ni, nj, state)) {
-                    state[i][j] = 2;
-                    return true;
+    private void bfs(Queue<int[]> q, boolean[][] ocean, int[][] heights) {
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0], c = cur[1];
+            ocean[r][c] = true;
+            for (int[] d : directions) {
+                int nr = r + d[0], nc = c + d[1];
+                if (nr >= 0 && nr < heights.length && nc >= 0 &&
+                    nc < heights[0].length && !ocean[nr][nc] &&
+                    heights[nr][nc] >= heights[r][c]) {
+                    q.add(new int[]{nr, nc});
                 }
             }
-            else if (ni < 0 || nj < 0) {
-                state[i][j] = 2;
-                return true;
-            }
         }
-
-        state[i][j] = 3;
-        return false;
-    }
-
-    boolean dfsA(int[][] nums, int i, int j, int[][] state) {
-
-        // Reached Atlantic
-        if (i >= nums.length || j >= nums[0].length) {
-            return true;
-        }
-
-        // Already computed
-        if (state[i][j] == 2) {
-            return true;
-        }
-
-        if (state[i][j] == 3) {
-            return false;
-        }
-
-        // Cycle
-        if (state[i][j] == 1) {
-            return false;
-        }
-
-        state[i][j] = 1;
-
-        int[][] dirs = {
-            {-1, 0},
-            {1, 0},
-            {0, -1},
-            {0, 1}
-        };
-
-        for (int[] d : dirs) {
-
-            int ni = i + d[0];
-            int nj = j + d[1];
-
-            if (ni >= 0 && ni < nums.length &&
-                nj >= 0 && nj < nums[0].length &&
-                nums[ni][nj] <= nums[i][j]) {
-
-                if (dfsA(nums, ni, nj, state)) {
-                    state[i][j] = 2;
-                    return true;
-                }
-            }
-            else if (ni >= nums.length || nj >= nums[0].length) {
-                state[i][j] = 2;
-                return true;
-            }
-        }
-
-        state[i][j] = 3;
-        return false;
     }
 }
