7a08b0022362b25e95c0021d94a94902f7013031
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-18 03:11:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.java
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -1,32 +1,16 @@
 class Solution {
-    public void floydWarshall(int[][] grid) {
-        int n = grid.length;
-        int INF = 100000000;
-
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         if (grid[i][j] == -1)
-        //             grid[i][j] = INF;
-        //     }
-        //     grid[i][i] = 0;
-        // }
-
-        for (int via = 0; via < n; via++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (grid[i][via] != INF && grid[via][j] != INF) {
-                        grid[i][j] = Math.min(grid[i][j],
-                                              grid[i][via] + grid[via][j]);
-                    }
-                }
-            }
-        }
-
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         if (grid[i][j] == INF)
-        //             grid[i][j] = -1;
-        //     }
-        // }
-    }
+	public void floydWarshall(int[][] dist) {
+		int n = dist.length;
+		int INF = 100000000;
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (dist[i][k] != INF && dist[k][j] != INF) {
+						dist[i][j] = Math.min(dist[i][j], dist[i][k]+dist[k][j]);
+					}
+				}
+			}
+		}
+		
+	}
 }
