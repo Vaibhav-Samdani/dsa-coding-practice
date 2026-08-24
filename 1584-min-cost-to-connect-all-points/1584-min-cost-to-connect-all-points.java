d32dd2cc884392f187db28e5892de3918b47c10b
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-25 04:56:03 +0530

    Time: 537 ms (29.9%), Space: 76.7 MB (57.63%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,6 +1,8 @@
 class Solution {
+
     int[] parent;
-	int[] rank;
+    int[] rank;
+
     class Edge {
         int u, v, wt;
 
@@ -12,69 +14,60 @@ class Solution {
     }
 
     public int minCostConnectPoints(int[][] points) {
-        ArrayList<Edge> edges = new ArrayList<>();
+        PriorityQueue<Edge> edges = new PriorityQueue<>((a,b)->Integer.compare(a.wt,b.wt));
 
-        for (int i = 0; i < points.length; i++) {
-            for (int j = i + 1; j < points.length; j++) {
-
+        for(int i = 0; i<points.length;i++){
+            for(int j = i+1; j<points.length;j++){
                 int dist = Math.abs(points[i][0] - points[j][0]) +
                         Math.abs(points[i][1] - points[j][1]);
 
-                edges.add(new Edge(i, j, dist));
+                edges.add(new Edge(i,j,dist));
             }
         }
 
-        	parent = new int[points.length];
+
+        parent = new int[points.length];
 		rank = new int[points.length];
 		for (int i = 0; i<points.length; i++) {
 			parent[i] = i;
 		}
-		edges.sort((a, b)->Integer.compare(a.wt, b.wt));
-		int ans = 0;
-		int edgesUsed = 0;
-		for (int i = 0; i<edges.size(); i++) {
-			int u = edges.get(i).u;
-			int v = edges.get(i).v;
-			int w = edges.get(i).wt;
-			
-			if (find(u) != find(v)) {
-				ans += w;
-				union(u, v);
-				edgesUsed++;
-				
-				if (edgesUsed == points.length - 1)
-					break;
-			}
-			
-		}
-		
-		return ans;
+
+        int ans = 0;
+
+        while(!edges.isEmpty()){
+            Edge curr = edges.poll();
+
+            if(find(curr.u) == find(curr.v)){
+                continue;
+            }else{
+                ans += curr.wt;
+                union(curr.u,curr.v);
+            }
+        }
 
 
+        return ans;
+
+    
     }
 
-    int find(int x) {
-		if (parent[x] == x) {
-			return x;
-		}
-		
-		return parent[x] = find(parent[x]);
-	}
-	
-	void union(int x, int y) {
-		int px = find(x);
-		int py = find(y);
-		
-		if (px == py)
-			return;
-		
-		if (rank[px] < rank[py]) {
-			parent[px] = py;
-		} else if (rank[px] > rank[py]) {
-			parent[py] = px;
-		} else {
-			parent[py] = px;
-			rank[px]++;
-		}
-	}
+    int find(int x){
+        if(x == parent[x]) return parent[x];
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void union(int x, int y){
+        int px = find(x);
+        int py = find(y);
+
+        if(rank[px] > rank[py]){
+            parent[py] = px;
+        }else if(rank[px] < rank[py]){
+              parent[px] = py;
+        }else{
+            parent[px] = py;
+            rank[py]++;
+        }
+    }
 }
