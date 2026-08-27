d8f6ad01e43852f19e6dcc79da71e5edf3265eb0
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-27 18:47:52 +0530

    Time: 0 ms (100%), Space: 42.9 MB (23.03%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,33 +1,33 @@
 class Solution {
+    int[] dp;
 
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
-        if (n == 2) return Math.max(nums[0], nums[1]);
-        int[] dp = new int[n + 2];
+        dp = new int[n];
+        Arrays.fill(dp, -1);
 
-        int firstVal = helper(dp, nums, 0, n - 2);
+        if (n <= 3) {
+            int ans = 0;
+            for (int i = 0; i < n; i++) {
+                ans = Math.max(nums[i], ans);
+            }
 
-        Arrays.fill(dp, 0);
-
-        int secondVal = helper(dp, nums, 1, n - 1);
-
-        return Math.max(firstVal, secondVal);
-    }
-
-    int helper(int[] dp, int[] nums, int i, int end) {
-        // int[] dp = new int[nums.length+2];
-        int next1 = 0;
-        int next2 = 0;
-        for (int j = end; j >= i; j--) {
-
-            int curr = Math.max(nums[j] + next2, next1);
-
-            next2 = next1;
-            next1 = curr;
-
+            return ans;
         }
 
-        return next1;
+        int first = solve(nums, 0, n - 1);
+        Arrays.fill(dp, -1);
+        int second = solve(nums, 1, n);
+
+        return Math.max(first, second);
+    }
+
+    int solve(int[] nums, int i, int n) {
+        if (i >= n) return 0;
+        if (dp[i] != -1) return dp[i];
+        int take = nums[i] + solve(nums, i + 2, n);
+        int notTake = solve(nums, i + 1, n);
+
+        return dp[i] = Math.max(take, notTake);
     }
 }
