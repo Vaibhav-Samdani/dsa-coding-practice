f2a1a1842e6de5549ab779b93cc85574da987eec
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-30 16:24:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -1,27 +1,35 @@
 class Solution {
-    static int[][] dp;
+    static int dp[][];
     static int matrixMultiplication(int arr[]) {
         dp = new int[arr.length][arr.length];
         for(int i = 0; i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
         return solve(arr,1,arr.length-1);
+        
     }
     
-    static int solve(int[] arr, int i , int j){
-        if(j == i) return 0;
+    static int solve(int[] arr, int i, int j){
+        
+        if(i==j) return 0;
+        
         if(dp[i][j] != -1) return dp[i][j];
+        
         int min = Integer.MAX_VALUE;
         
-        for(int k = i; k < j;k++){
-            int c1 = solve(arr,i,k);
-            int c2 = solve(arr,k+1,j);
+        for(int k = i; k<j ; k++){
+            int left = solve(arr,i,k);
+            int right = solve(arr,k+1,j);
             
-            int cost = c1 + c2 + arr[i-1] * arr[k] * arr[j];
+            int cost = arr[i-1] * arr[k] * arr[j];
             
-            min = Math.min(cost,min);
+            int totalCost = cost + left + right;
+            
+            min = Math.min(totalCost, min);
         }
         
-        return dp[i][j] = min;
+        return dp[i][j]=  min;
+        
+        
     }
 }
