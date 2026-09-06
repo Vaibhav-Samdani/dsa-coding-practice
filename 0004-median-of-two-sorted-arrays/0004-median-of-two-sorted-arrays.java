9d7a447f763b61fdaa374453d5c24a03926f3c48
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-09-06 17:18:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,43 +1,40 @@
 class Solution {
-
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
+        int m = nums1.length, n = nums2.length;
 
         if (m > n) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
-        int l1 = 0;
-        int r1 = 0;
+        int l = 0;
+        int h = m;
 
-        int l2 = 0;
-        int r2 = 0;
+        while (l <= h) {
+            int Px = l + (h - l) / 2;
+            int Py = (m + n + 1) / 2 - Px;
 
-        int low = 0, high = m;
-        while (low <= high) {
-            int cut1 = low + (high - low) / 2;
-            int cut2 = (m + n + 1) / 2 - cut1;
-            l1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            r1 = cut1 == m ? Integer.MAX_VALUE : nums1[cut1];
+            // Left wale
+            int l1 = Px == 0 ? Integer.MIN_VALUE : nums1[Px-1];
+            int l2 = Py == 0 ? Integer.MIN_VALUE :nums2[Py-1];
 
-            l2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            r2 = cut2 == n ? Integer.MAX_VALUE : nums2[cut2];
+            // Right wale
+            int r1 = Px == m ? Integer.MAX_VALUE :nums1[Px];
+            int r2 = Py == n ? Integer.MAX_VALUE :nums2[Py];
 
-            if (l1 <= r2 && l2 <= r1) {
-                if ((m + n) % 2 == 1) {
-                    return (double) Math.max(l1, l2);
-                } else {
-                    double val = (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-                    return (double) (val);
+            if(l1<=r2 && l2 <= r1){
+                if( (m + n) % 2 == 1 ){
+                    return Math.max(l1,l2);
+                }else{
+                    return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
                 }
-            } else if (r1 < l2) {
-                low = cut1 + 1;
-            } else {
-                high = cut1 - 1;
+            }else if(l2>=r1){
+                l = Px +1;
+            }else{
+                h = Px - 1;
             }
+
         }
 
-        return 0.0;
+        return -1;
     }
 }
