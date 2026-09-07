5a408170aadf68d163443f8db86ba4dc890abe71
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-09-08 01:18:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,34 +1,38 @@
 class Solution {
-
     public int minEatingSpeed(int[] piles, int h) {
         int low = 1;
+
         int high = 0;
-
-        for (int i = 0; i < piles.length; i++) {
-            high = Math.max(piles[i], high);
+        
+        for(int i = 0; i<piles.length;i++){
+            high = Math.max(high,piles[i]);
         }
-
-        high++;
-
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (isPossible(piles, h, mid)) {
-                high = mid;
-            } else {
-                low = mid + 1;
+        
+        int ans = 0;
+        // high++;
+        while(low < high){
+            int mid = low + (high-low)/2;
+            
+            if(isPossible(piles,h,mid)){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
-        return low;
+        
+        return ans;
     }
 
-    boolean isPossible(int[] piles, int h, int mid) {
-        int i = 0;
-
-        while (i < piles.length) {
-            h -= (piles[i] + mid - 1) / mid;
-            i++;
+    static boolean isPossible(int[] nums,int h, int mid){
+        
+        for(int i = 0; i<nums.length;i++){
+            h = h - (nums[i] % mid == 0? nums[i] / mid : (nums[i] / mid)+1);
         }
+        
         return h >= 0;
+        
     }
+
+
 }
