8ef4cb70dd0c7324c807419b3ed2f92b3dc61310
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-09-17 05:02:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,36 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length;
+        int n = nums1.length;
+        int m = nums2.length;
 
-        if (m > n) {
+        if (m < n)
             return findMedianSortedArrays(nums2, nums1);
-        }
 
-        int l = 0;
-        int h = m;
+        int left = 0, right = n;
 
-        while (l <= h) {
-            int Px = l + (h - l) / 2;
-            int Py = (m + n + 1) / 2 - Px;
+        while (left <= right) {
+            int mid1 = left + (right - left) / 2;
+            int mid2 = (m + n + 1) / 2 - mid1;
 
-            // Left wale
-            int l1 = Px == 0 ? Integer.MIN_VALUE : nums1[Px-1];
-            int l2 = Py == 0 ? Integer.MIN_VALUE :nums2[Py-1];
+            int l1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2 - 1];
 
-            // Right wale
-            int r1 = Px == m ? Integer.MAX_VALUE :nums1[Px];
-            int r2 = Py == n ? Integer.MAX_VALUE :nums2[Py];
+            int r1 = (mid1 == n) ? Integer.MAX_VALUE : nums1[mid1];
+            int r2 = (mid2 == m) ? Integer.MAX_VALUE : nums2[mid2];
 
-            if(l1<=r2 && l2 <= r1){
-                if( (m + n) % 2 == 1 ){
+            if (l1 <= r2 && l2 <= r1) {
+
+                if ((n + m) % 2 == 1) {
                     return Math.max(l1,l2);
-                }else{
-                    return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
+                } else {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
                 }
-            }else if(l2>=r1){
-                l = Px +1;
-            }else{
-                h = Px - 1;
+            } else if (l2 > r1) {
+                left = mid1 + 1;
+            } else {
+                right = mid1 - 1;
             }
 
         }
