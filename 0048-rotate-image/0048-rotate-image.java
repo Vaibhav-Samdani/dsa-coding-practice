74ac61ac6750b1e1bbba91ac201ff92d8781cfe0
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-09-21 02:48:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -4,11 +4,11 @@ class Solution {
         // 1. Transpose
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                if(i <= j){
+                
                     int temp = matrix[i][j];
                     matrix[i][j] = matrix[j][i];
                     matrix[j][i] = temp;
-                }
+                
             }
         }
 
