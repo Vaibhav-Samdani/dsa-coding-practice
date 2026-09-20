243ef2d862091bc088860e3cdf84d3ed8e9341be
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-09-21 02:48:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,8 +1,10 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (i <= j) {
+        int n = matrix.length;
+        // 1. Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if(i <= j){
                     int temp = matrix[i][j];
                     matrix[i][j] = matrix[j][i];
                     matrix[j][i] = temp;
@@ -10,13 +12,22 @@ class Solution {
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length / 2; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix[0].length-j-1];
-                matrix[i][matrix[0].length-j-1] = temp;
+
+        // 2. Reverse; 
+        for (int i = 0; i < n; i++) {
+            int j = 0, k = n-1;
+            while(j<k){
+                swap(matrix[i],j,k);
+                j++;
+                k--;
             }
         }
 
+    }
+
+    void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
