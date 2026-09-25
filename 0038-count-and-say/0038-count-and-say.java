3aa0881fb78a4fab7fea018b905760eeed3be68e
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-09-25 16:12:02 +0530

    Time: 20 ms (7.13%), Space: 43.2 MB (28.5%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -5,9 +5,9 @@ class Solution {
 
         for (int i = 2; i <= n; i++) {
             StringBuilder curr = new StringBuilder();
-            int count = 0;
-            for (int j = 0; j < st.length(); j++) {
-                if(j==0 || st.charAt(j) == st.charAt(j-1)){
+            int count = 1;
+            for (int j = 1; j < st.length(); j++) {
+                if(st.charAt(j) == st.charAt(j-1)){
                     count++;
                 }else{
                    curr.append(count);
